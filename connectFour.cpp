2887800a29c3252cc16482a60c15e944c82a84a9
dineshsrisai <dineshsrisai1@gmail.com>
2026-08-05 14:48:20 +0530

    printBoard

--- connectFour.cpp
+++ connectFour.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 const string RED = "\033[1;31m";
@@ -6,7 +6,23 @@ const string YELLOW = "\033[1;33m";
 const string BLUE = "\033[1;34m";
 const string RESET = "\033[0m";
 
+const int R = 6, C = 7;
+char b[R][C];
 
-int main() {
+void printBoard()
+{
+    cout << "\n";
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            cout << b[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+int main()
+{
+    printBoard();
     return 0;
 }
